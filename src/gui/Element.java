8f059ca9ba28088405bc8b525b9b353256b1f618
Matija <programer.kevic@gmail.com>
2017-06-22 17:42:17 +0200

    Initial Commit

---
+++ src/gui/Element.java
@@ -0,0 +1,40 @@
+package gui;
+
+import game.Renderer;
+
+public abstract class Element {
+    private int x, y;
+
+    public Element() {
+
+    }
+
+    public abstract void update();
+
+    public abstract void render(Renderer renderer);
+
+    public abstract int getWidth();
+
+    public abstract int getHeight();
+
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void onKey(boolean[] keys) {
+
+    }
+}
